e4470a6298df6e1a79f8a33627a0accbdd01f273
Andy Hudson <andyhudson725@gmail.com>
2021-08-08 17:07:56 -0500

    Added webpacker to yarn

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,7 +26,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
+   # Production depends on precompilation of packs prior to booting for performance.
   config.assets.compile = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
